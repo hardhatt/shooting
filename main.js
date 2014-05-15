20f624be94c4d55178fc910ba94a9233a5a8cd70
hardhatt <kouzitomasao@yahoo.co.jp>
2014-05-16 01:13:44 +0900

    made background

---
+++ main.js
@@ -0,0 +1,24 @@
+enchant();
+
+window.onload = function(){
+	core = new Game(320, 320);
+	core.preload("map0.png");
+	core.fps = 15;
+	Scene = core.rootScene;
+	
+	core.onload = function(){
+		//背景の生成---------------------------------------------
+		var bg = new Sprite(320, 320);
+		var maptip = core.assets["map0.png"];
+		var image = new Surface(320, 320);
+		for(var j = 0; j <= 320; j += 16){
+			for(var i = 0; i <= 320; i += 16){
+				image.draw(maptip, 32, 0, 16, 16, i, j, 16, 16);
+			}
+		}
+		bg.image = image;
+		Scene.addChild(bg);
+		//------------------------------------------------------
+	}
+	core.start();		//ゲーム開始
+};
